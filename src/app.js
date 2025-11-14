173c283bc382bed28923fa0f3e7e4b3dc3f3ca64
wellborgmann <wellborgmann@gmail.com>
2025-11-13 23:17:04 -0300

    Atualizações do projeto

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.get("/home", autenticarToken, (req, res) => {
   res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
-app.get("/pix", autenticarToken, (req, res) => {
+app.get("/pix",  (req, res) => {
   res.sendFile(path.join(__dirname, "views", "pix.html"));
 });
 

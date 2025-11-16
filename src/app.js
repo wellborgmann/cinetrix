8b31c18fa59277dd99c152e97a535982e366c8d9
wellborgmann <wellborgmann@gmail.com>
2025-11-16 00:32:20 -0300

    tv

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 // Middlewares globais
+app.use(cors()); 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
